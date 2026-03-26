ec0ba81ca893f3d89726323b84b896bd2c77730e
Juan Pablo Ruacho Rios <ruacho73@gmail.com>
2026-03-26 03:04:48 -0700

    se crearon las pantallas necesarias para el desarrollo de la aplicacion, asi tambien se quitaron los comentarios del main.dart

---
+++ nutricion/lib/planScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PlanScreen extends StatefulWidget {
+  const PlanScreen({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<PlanScreen> createState() => _PlanScreenState();
+}
+
+class _PlanScreenState extends State<PlanScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
